138a044ebb77cea44f4e4860904a0e31dc5bec39
jyobb <jyobb1@gmail.com>
2016-07-19 14:13:10 -0600

    mapquest changes

--- js/open_map_GEOJSON_location_config.js
+++ js/open_map_GEOJSON_location_config.js
@@ -54,15 +54,16 @@ function initialize() {
   // Create map.
   map = L.map('map_canvas', {
     // console.log("centre: " + lat + lon);.
+	  layers:MQ.mapLayer(),
     center: [lat, lon],
         minZoom: 2,
         zoom: 8
   });
 
-  L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
-    subdomains: ['otile1','otile2','otile3','otile4']
-            }).addTo(map);
+  //L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
+  //      attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+  //subdomains: ['otile1','otile2','otile3','otile4']
+  //        }).addTo(map);
 
   var markers = L.markerClusterGroup();
 
